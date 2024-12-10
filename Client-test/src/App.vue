e6dfa6d2d4020bfc21eba855cfd02adac45f0552
Lunaries-Olympians <haryrahmad9@gmail.com>
2024-12-10 15:32:11 +0700

    feat:ref

--- Client-test/src/App.vue
+++ Client-test/src/App.vue
@@ -1,4 +1,4 @@
-<script setup >
+<script setup>
 import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
@@ -16,57 +16,150 @@ import HelloWorld from "./components/HelloWorld.vue";
       </div>
     </header>
 
-    <section class="bg-blue-900 text-white text-center py-8">
-      <div id="indicators-carousel" class="relative w-full" data-carousel="static">
+    <section class=" text-white text-center py-8">
+      <div
+        id="indicators-carousel"
+        class="relative w-full"
+        data-carousel="static"
+      >
+        <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
+          <div
+            class="hidden duration-700 ease-in-out"
+            data-carousel-item="active"
+          >
+            <img
+              src="https://gratisography.com/wp-content/uploads/2024/01/gratisography-cyber-kitty-800x525.jpg"
+              class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              alt="..."
+            />
+          </div>
 
-    <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
+          <div class="hidden duration-700 ease-in-out" data-carousel-item>
+            <img
+              src="https://img.freepik.com/free-photo/buddha-bowl-dish-with-vegetables-legumes-top-view_1150-42589.jpg"
+              class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              alt="..."
+            />
+          </div>
 
-        <div class="hidden duration-700 ease-in-out" data-carousel-item="active">
-            <img src="https://gratisography.com/wp-content/uploads/2024/01/gratisography-cyber-kitty-800x525.jpg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+          <div class="hidden duration-700 ease-in-out" data-carousel-item>
+            <img
+              src="https://t4.ftcdn.net/jpg/01/20/28/25/360_F_120282530_gMCruc8XX2mwf5YtODLV2O1TGHzu4CAb.jpg"
+              class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              alt="..."
+            />
+          </div>
+
+          <div class="hidden duration-700 ease-in-out" data-carousel-item>
+            <img
+              src="https://img.freepik.com/free-photo/buddha-bowl-dish-with-vegetables-legumes-top-view_1150-42589.jpg"
+              class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              alt="..."
+            />
+          </div>
+
+          <div class="hidden duration-700 ease-in-out" data-carousel-item>
+            <img
+              src="https://t4.ftcdn.net/jpg/01/20/28/25/360_F_120282530_gMCruc8XX2mwf5YtODLV2O1TGHzu4CAb.jpg"
+              class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              alt="..."
+            />
+          </div>
         </div>
 
-        <div class="hidden duration-700 ease-in-out" data-carousel-item>
-            <img src="https://img.freepik.com/free-photo/buddha-bowl-dish-with-vegetables-legumes-top-view_1150-42589.jpg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+        <div
+          class="absolute z-30 flex -translate-x-1/2 space-x-3 rtl:space-x-reverse bottom-5 left-1/2"
+        >
+          <button
+            type="button"
+            class="w-3 h-3 rounded-full"
+            aria-current="true"
+            aria-label="Slide 1"
+            data-carousel-slide-to="0"
+          ></button>
+          <button
+            type="button"
+            class="w-3 h-3 rounded-full"
+            aria-current="false"
+            aria-label="Slide 2"
+            data-carousel-slide-to="1"
+          ></button>
+          <button
+            type="button"
+            class="w-3 h-3 rounded-full"
+            aria-current="false"
+            aria-label="Slide 3"
+            data-carousel-slide-to="2"
+          ></button>
+          <button
+            type="button"
+            class="w-3 h-3 rounded-full"
+            aria-current="false"
+            aria-label="Slide 4"
+            data-carousel-slide-to="3"
+          ></button>
+          <button
+            type="button"
+            class="w-3 h-3 rounded-full"
+            aria-current="false"
+            aria-label="Slide 5"
+            data-carousel-slide-to="4"
+          ></button>
         </div>
 
-        <div class="hidden duration-700 ease-in-out" data-carousel-item>
-            <img src="https://t4.ftcdn.net/jpg/01/20/28/25/360_F_120282530_gMCruc8XX2mwf5YtODLV2O1TGHzu4CAb.jpg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
-        </div>
-
-        <div class="hidden duration-700 ease-in-out" data-carousel-item>
-            <img src="https://img.freepik.com/free-photo/buddha-bowl-dish-with-vegetables-legumes-top-view_1150-42589.jpg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
-        </div>
-
-        <div class="hidden duration-700 ease-in-out" data-carousel-item>
-            <img src="https://t4.ftcdn.net/jpg/01/20/28/25/360_F_120282530_gMCruc8XX2mwf5YtODLV2O1TGHzu4CAb.jpg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
-        </div>
-    </div>
-
-    <div class="absolute z-30 flex -translate-x-1/2 space-x-3 rtl:space-x-reverse bottom-5 left-1/2">
-        <button type="button" class="w-3 h-3 rounded-full" aria-current="true" aria-label="Slide 1" data-carousel-slide-to="0"></button>
-        <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
-        <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
-        <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 4" data-carousel-slide-to="3"></button>
-        <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 5" data-carousel-slide-to="4"></button>
-    </div>
-
-    <button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
-        <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-            <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
+        <button
+          type="button"
+          class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+          data-carousel-prev
+        >
+          <span
+            class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none"
+          >
+            <svg
+              class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 6 10"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M5 1 1 5l4 4"
+              />
             </svg>
             <span class="sr-only">Previous</span>
-        </span>
-    </button>
-    <button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
-        <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-            <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+          </span>
+        </button>
+        <button
+          type="button"
+          class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+          data-carousel-next
+        >
+          <span
+            class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none"
+          >
+            <svg
+              class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 6 10"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="m1 9 4-4-4-4"
+              />
             </svg>
             <span class="sr-only">Next</span>
-        </span>
-    </button>
-</div>
+          </span>
+        </button>
+      </div>
     </section>
 
     <section class="bg-white shadow mt-6 p-6 rounded">
@@ -74,34 +167,73 @@ import HelloWorld from "./components/HelloWorld.vue";
         <h2 class="text-lg font-semibold">Your Level</h2>
         <a href="#" class="text-blue-500 hover:underline">Learn More</a>
       </div>
-      <div class="flex justify-between mt-6">
-        <div class="text-center">
-          <div class="bg-gray-100 py-2 px-4 rounded-full">Silver</div>
-        </div>
-        <div class="text-center">
-          <div class="bg-gray-100 py-2 px-4 rounded-full">Gold</div>
-        </div>
-        <div class="text-center">
-          <div class="bg-gray-100 py-2 px-4 rounded-full">Platinum</div>
+
+      <div class="flex justify-center items-center h-full w-full mt-6">
+        <div class="flex justify-center items-center">
+          <ol
+            class="flex items-center w-full mb-4 sm:mb-5 justify-center mx-auto"
+          >
+            <li
+              class="flex w-full items-center text-blue-600 dark:text-blue-500 after:content-[''] after:w-full after:h-1 after:border-b after:border-blue-100 after:border-4 after:inline-block dark:after:border-blue-800"
+            >
+              <div
+                class="flex items-center justify-center w-20 h-20 bg-blue-100 rounded-full lg:h-12 lg:w-12 dark:bg-blue-800 shrink-0"
+              >
+                <p class="text-lg font-semibold">Silver</p>
+              </div>
+            </li>
+
+            <li
+              class="flex w-full items-center after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-100 after:border-4 after:inline-block dark:after:border-gray-700"
+            >
+              <div
+                class="flex items-center justify-center w-20 h-20 bg-gray-100 rounded-full lg:h-12 lg:w-12 dark:bg-gray-700 shrink-0"
+              >
+                <p class="text-lg font-semibold">Gold</p>
+              </div>
+            </li>
+
+            <li class="flex items-center w-full">
+              <div
+                class="flex items-center justify-center w-20 h-20 bg-gray-100 rounded-full lg:h-12 lg:w-12 dark:bg-gray-700 shrink-0"
+              >
+                <p class="text-lg font-semibold">Platinum</p>
+              </div>
+            </li>
+          </ol>
         </div>
       </div>
       <p class="text-center text-sm mt-4 text-gray-600">
-        Spend 
+        Spend
+        <span class="font-bold text-black">3,500,001</span> by
+        <span class="font-bold text-black">23 Nov 2024</span> to reach
+        <span class="text-yellow-500">Gold</span>
       </p>
-      <div class="flex justify-between items-center mt-6">
-        <span>Bakmi GM Points</span>
-        <span class="font-bold text-lg">40</span>
+      <div class="relative w-full mt-4">
+        <div class="bg-gray-200 h-2 rounded-full">
+          <div class="bg-yellow-400 h-2 rounded-full" style="width: 40%"></div>
+        </div>
       </div>
     </section>
 
-    <section class="bg-blue-900 text-white text-center py-8">
+    <section class="bg-white shadow mt-6 p-6 rounded">
+      <div class="flex justify-between items-center">
+        <span class="text-gray-700 font-medium">Bakmi GM Points</span>
+        <span class="font-bold text-lg">40</span>
+      </div>
+      <p class="text-sm text-gray-600 mt-2">
+        40 will expire on <span class="font-bold text-black">22 Feb 2024</span>
+        <a href="#" class="text-blue-500 hover:underline">See Details</a>
+      </p>
+    </section>
+
+    <section class="bg-blue-900 rounded-lg text-white text-center py-8 my-5">
       <h1 class="text-3xl font-bold">Roll with Patrol</h1>
       <div class="flex justify-center space-x-6 mt-6">
         <img
           src="https://cdn-icons-png.flaticon.com/512/6485/6485869.png"
           alt="Sign Up Reward"
           class="w-24 h-24 mr-4"
- 
         />
         <div class="p-4">
           <h2 class="text-lg font-bold flex items-center space-x-2">
